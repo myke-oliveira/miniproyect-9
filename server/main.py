aef19c043d4ba0130c78e8ccc57436c6a392be7e
Myke Albuquerque Pinto de Oliveira <mykeapo@gmail.com>
2023-07-09 15:52:25 -0300

    first aproach

---
+++ server/main.py
@@ -0,0 +1,65 @@
+import os
+from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
+from dotenv import load_dotenv
+import openai
+import json
+
+app = Flask(__name__)
+CORS(app)
+
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = {'mp3', 'mp4', 'mpeg', 'mpga',
+                      'm4a', 'wav', 'webm'}
+
+app.config['CORS_HEADERS'] = 'Content-Type'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['DEBUG'] = True
+
+openai.organization = "org-9XRfyx9yvMlDpr6CNmFHWXKY"
+openai.api_key = os.getenv('OPENAI_API_KEY')
+
+
+@app.route('/', methods=['POST'])
+@cross_origin()
+def upload_file():
+    print(request.files)
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file part'})
+
+    file = request.files['file']
+
+    if file.filename == '':
+        return jsonify({'error': 'No selected file'})
+
+    if not allowed_file(file.filename):
+        return jsonify({'error': 'Format not valid.'})
+
+    filename = file.filename
+    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    transcripted_number = transcript_number(filename)
+    return transcripted_number
+
+
+def allowed_file(filename):
+    return any(map(lambda extention: filename.endswith(extention), ALLOWED_EXTENSIONS))
+
+
+def transcript_number(filename):
+    filepath = os.path.join(UPLOAD_FOLDER, filename)
+    with open(filepath, 'rb') as audiofile:
+        transcript = openai.Audio.transcribe("whisper-1", audiofile)
+    data = json.loads(str(transcript))
+    text = data['text']
+    
+    try:
+        float(text)
+        print('ok')
+        return transcript
+    except ValueError:
+        print('erro')
+        return '{ "error": "Number not found" }'
+
+if __name__ == '__main__':
+    load_dotenv()
+    app.run()
---
+++ site/index.js
@@ -0,0 +1,33 @@
+console.log('start')
+
+document.querySelector('input[type="submit"]').addEventListener('click', async function (event) {
+  event.preventDefault();
+
+  const file = document.querySelector('input[type="file"]').files[0];
+
+  const formData = new FormData()
+  formData.append('file', file)
+
+  const URL = "http://localhost:5000"
+
+  try {
+    const response = await fetch(URL, {
+      header: {
+        "Content-Type": "multipart/form-data"
+      },
+      body: formData,
+      method: "POST"
+    })
+
+    const data = await response.json()
+
+    const html = data.error
+      ? `<p class="fs-1"><strong>Error:</strong> ${data.error}</p>`
+      : `<p class="fs-1"><strong>Result:</strong> ${data.text}</p>`
+
+    document.querySelector('#result').innerHTML = html
+  } catch (error) {
+    console.log(error)
+  }
+  
+})
